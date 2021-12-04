a2e578ff7d9640e86639a753d76d81c9552a94fc
EduardoRFS <theeduardorfs@gmail.com>
2021-12-04 16:54:11 +0000

    turn

--- contract/turn.ml
+++ contract/turn.ml
@@ -9,3 +9,17 @@ let current ~level =
   match levels_since_level / round_time with
   | Some (turn, _remainder) -> turn
   | None -> assert false
+
+type turn_kind = Committer | Fork_committer | Rejector | Fork_rejector
+
+let turns_per_round = 4n
+let turn_kind ~level =
+  let current_turn = current ~level in
+  (* C -> F_C -> R -> F_R *)
+  match current_turn / turns_per_round with
+  | Some (_, remainder) ->
+      if remainder = 0n then Committer
+      else if remainder = 1n then Fork_committer
+      else if remainder = 2n then Rejector
+      else Fork_rejector
+  | None -> assert false
--- contract/turn.mli
+++ contract/turn.mli
@@ -3,4 +3,10 @@ open Environment
 type t = nat
 
 (* O(1) *)
-val current : level:t -> t
+val current : level:level -> t
+
+val turns_per_round : t
+type turn_kind = Committer | Fork_committer | Rejector | Fork_rejector
+
+(* O(1) *)
+val turn_kind : level:level -> turn_kind
