3204c8ad4c8bf216874de6a05247c31afa3a1f22
EduardoRFS <theeduardorfs@gmail.com>
2021-12-27 21:05:42 -0300

    simple rejection game

---
+++ contract/simple_rejection_game.mli
@@ -0,0 +1,32 @@
+open Environment
+
+type state
+type t = state
+
+type move =
+  | Move_handshake of { final_state_hash : state_hash }
+  | Move_mid_state_hash of { mid_state_hash : state_hash }
+  | Move_replay of { vm_state : Vm.t }
+type move_result =
+  | Move_result_winner of player
+  | Move_result_waiting of state
+  | Move_result_invalid
+
+val play :
+  previous_state_hash:state_hash ->
+  committer_steps:steps ->
+  committer_state_hash:state_hash ->
+  rejector_steps:steps ->
+  rejector_mid_state_hash:state_hash ->
+  t
+
+val move : player -> move -> state -> move_result
+
+(* used by the forkable game *)
+type search_state = private {
+  initial_step : steps;
+  initial_state_hash : state_hash;
+  final_step : steps;
+  final_state_hash : state_hash;
+}
+val search_state : state -> search_state option
