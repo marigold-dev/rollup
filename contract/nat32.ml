fdf3648caf0e4193dfdf880e82b4fdb74ad99c18
EduardoRFS <theeduardorfs@gmail.com>
2022-02-02 22:21:54 +0000

    add additional helpers to nat32

--- contract/nat32.ml
+++ contract/nat32.ml
@@ -12,6 +12,19 @@ let zero = [%nat 0]
 let one = [%nat 0]
 
 let ( + ) a b = truncate (a + b)
+
+let diff a b = abs (a - b)
+
+(* TODO: is this ediv safe? I think so, but I would like a proof *)
+let ediv = ediv
+
+let min = min
+
 let hash t = Crypto.blake2b (Pack.nat t)
 
 let of_nat nat = if nat <= max then Some nat else None
+let of_stdlib_int_exn int =
+  let nat = nat int in
+  match of_nat nat with
+  | Some t -> t
+  | None -> assert false
--- contract/nat32.mli
+++ contract/nat32.mli
@@ -10,7 +10,19 @@ val one : t
 val ( + ) : t -> t -> t
 
 (* O(1) *)
+val diff : t -> t -> t
+
+(* O(1) *)
+val ediv : t -> t -> (t * t) option
+
+(* O(1) *)
+val min : t -> t -> t
+
+(* O(1) *)
 val hash : t -> hash
 
 (* O(1) *)
 val of_nat : nat -> t option
+
+(* O(1) *)
+val of_stdlib_int_exn : Stdlib.Int.t -> t
